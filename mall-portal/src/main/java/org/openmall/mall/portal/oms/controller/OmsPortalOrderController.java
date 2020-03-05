3293042573a8dbb8d2369497eec669efb375e3f0
bobo <bobolau@live.com>
2020-03-05 20:16:33 +0800

    fix API注解

--- mall-portal/src/main/java/org/openmall/mall/portal/oms/controller/OmsPortalOrderController.java
+++ mall-portal/src/main/java/org/openmall/mall/portal/oms/controller/OmsPortalOrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
  * 1. 购物车生成订单时可选择部分商品下单
  * 2. 直接选择某商品生成订单
  */
-@Controller
+@RestController
 @Api(tags = "OmsPortalOrderController",description = "订单管理")
 @RequestMapping("/order")
 public class OmsPortalOrderController {
@@ -27,35 +27,29 @@ public class OmsPortalOrderController {
     private OmsPortalOrderService portalOrderService;
     @ApiOperation("根据购物车信息生成确认单信息")
     @RequestMapping(value = "/generateConfirmOrder",method = RequestMethod.POST)
-    @ResponseBody
     public CommonResult<ConfirmOrderResult> generateConfirmOrder(){
         ConfirmOrderResult confirmOrderResult = portalOrderService.generateConfirmOrder();
         return CommonResult.success(confirmOrderResult);
     }
-
     @ApiOperation("根据购物车信息生成订单")
     @RequestMapping(value = "/generateOrder",method = RequestMethod.POST)
-    @ResponseBody
-    public Object generateOrder(@RequestBody OrderParam orderParam){
+    public CommonResult generateOrder(@RequestBody OrderParam orderParam){
         return portalOrderService.generateOrder(orderParam);
     }
     @ApiOperation("支付成功的回调")
     @RequestMapping(value = "/paySuccess",method = RequestMethod.POST)
-    @ResponseBody
-    public Object paySuccess(@RequestParam Long orderId){
+    public CommonResult paySuccess(@RequestParam Long orderId){
         return portalOrderService.paySuccess(orderId);
     }
 
     @ApiOperation("自动取消超时订单")
     @RequestMapping(value = "/cancelTimeOutOrder",method = RequestMethod.POST)
-    @ResponseBody
-    public Object cancelTimeOutOrder(){
+    public CommonResult cancelTimeOutOrder(){
         return portalOrderService.cancelTimeOutOrder();
     }
 
     @ApiOperation("取消单个超时订单")
     @RequestMapping(value = "/cancelOrder",method = RequestMethod.POST)
-    @ResponseBody
     public CommonResult cancelOrder(Long orderId){
         portalOrderService.sendDelayMessageCancelOrder(orderId);
         return CommonResult.success(null);
