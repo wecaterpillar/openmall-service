1c652daceaa2291ecbdc6fff28176ad0027515e4
bobo <bobolau@live.com>
2020-03-30 17:37:44 +0800

    update

--- mall-business/mall-base/base-core/src/main/java/org/openmall/mall/base/model/Channel.java
+++ mall-business/mall-base/base-core/src/main/java/org/openmall/mall/base/model/Channel.java
@@ -10,9 +10,14 @@ public class Channel {
     private Long merchantCode;  //所属商户
     private Long ChannelType;  //渠道类型
     //国家
+    private String countryCode;
+    private String countryName;
     //语言
     private String language;   //当前语言/默认语言
     private String languages;  //可使用语言，逗号分割
     //货币
+    private String curCode;
+    private String curName;
+    private String curCodes;  //可使用货币，逗号分割
 
 }
--- mall-business/mall-base/base-core/src/main/java/org/openmall/mall/base/service/ChannelService.java
+++ mall-business/mall-base/base-core/src/main/java/org/openmall/mall/base/service/ChannelService.java
@@ -1,8 +1,17 @@
 package org.openmall.mall.base.service;
 
+import java.util.Map;
+
 /**
  * 业务渠道服务API
  */
 public interface ChannelService {
+
+    /**
+     * 获取渠道配置信息
+     * @param channelId
+     * @return
+     */
+    Map<String, String> getChannelConfig(String channelId);
 
 }
