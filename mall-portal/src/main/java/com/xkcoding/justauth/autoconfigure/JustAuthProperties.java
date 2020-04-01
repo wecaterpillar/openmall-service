e9a0489b2ec64611a4e5b6269d8c6f1247f6dd07
bobo <bobolau@live.com>
2020-04-01 19:53:37 +0800

    集成auth第三方用户登录

---
+++ mall-portal/src/main/java/com/xkcoding/justauth/autoconfigure/JustAuthProperties.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019-2029, xkcoding & Yangkai.Shen & 沈扬凯 (237497819@qq.com & xkcoding.com).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xkcoding.justauth.autoconfigure;
+
+import lombok.Getter;
+import lombok.Setter;
+import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.config.AuthDefaultSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * JustAuth自动装配配置类
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-07-22 10:59
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "justauth")
+public class JustAuthProperties {
+    /**
+     * 是否启用 JustAuth
+     */
+    private boolean enabled;
+
+    /**
+     * JustAuth 默认配置
+     */
+    private Map<AuthDefaultSource, AuthConfig> type = new HashMap<>();
+
+    /**
+     * JustAuth 自定义配置
+     */
+    @NestedConfigurationProperty
+    private ExtendProperties extend;
+
+    /**
+     * 缓存配置类
+     */
+    @NestedConfigurationProperty
+    private CacheProperties cache = new CacheProperties();
+
+}
---
+++ mall-portal/src/main/java/org/openmall/mall/portal/config/extend/ExtendSource.java
@@ -0,0 +1,103 @@
+package org.openmall.mall.portal.config.extend;
+
+import me.zhyd.oauth.config.AuthSource;
+
+/**
+ * <p>
+ * 扩展的自定义 source
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019/10/9 14:14
+ */
+public enum ExtendSource implements AuthSource {
+    /**
+     * 企业微信
+     */
+    WEB_ENTERPRISE_WE_CHAT {
+        /**
+         * 授权的api
+         *
+         * @return url
+         */
+        @Override
+        public String authorize() {
+            return "https://open.weixin.qq.com/connect/oauth2/authorize";
+        }
+
+        /**
+         * 获取accessToken的api
+         *
+         * @return url
+         */
+        @Override
+        public String accessToken() {
+            return "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+        }
+
+        /**
+         * 获取用户信息的api
+         *
+         * @return url
+         */
+        @Override
+        public String userInfo() {
+            return "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo";
+        }
+    },
+
+    /**
+     * 测试
+     */
+    TEST {
+        /**
+         * 授权的api
+         *
+         * @return url
+         */
+        @Override
+        public String authorize() {
+            return "http://authorize";
+        }
+
+        /**
+         * 获取accessToken的api
+         *
+         * @return url
+         */
+        @Override
+        public String accessToken() {
+            return "http://accessToken";
+        }
+
+        /**
+         * 获取用户信息的api
+         *
+         * @return url
+         */
+        @Override
+        public String userInfo() {
+            return null;
+        }
+
+        /**
+         * 取消授权的api
+         *
+         * @return url
+         */
+        @Override
+        public String revoke() {
+            return null;
+        }
+
+        /**
+         * 刷新授权的api
+         *
+         * @return url
+         */
+        @Override
+        public String refresh() {
+            return null;
+        }
+    }
+}
